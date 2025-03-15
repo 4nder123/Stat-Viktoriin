16f2d62503bf91afccd7d0fc640cc28096632b43
4nder123 <ander.jarv@gmail.com>
2025-03-15 21:20:03 +0200

    Project base files

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <Viktoriin/>
+</template>
+
+<script>
+import Viktoriin from './components/Viktoriin.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Viktoriin
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/Viktoriin.vue
@@ -0,0 +1,11 @@
+<template>
+</template>
+
+<script>
+export default {
+  name: 'Viktoriin',
+}
+</script>
+
+<style scoped>
+</style>
